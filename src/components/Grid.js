5b4d09a3ee760d3de1142f9fccabc0e113665ce8
Matias Paradela <paradelamatias@gmail.com>
2023-02-20 11:41:41 -0300

    Add random functionality when the Grid of Cards is rendered

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -13,13 +13,24 @@ const names = [
 ]
 
 function renderCardsRandomly() {
+  const randomNames = shuffleArray(names.slice(0));
   
-  
-  const listToRender = names.map((elem, index) => 
+  const listToRender = randomNames.map((elem, index) => 
       <Card name={elem} key={index} />
     )
   
   return listToRender;
+}
+
+function shuffleArray(arr) {
+  for (let i = arr.length-1; i>0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+  }
+
+  return arr;
 }
 
 export default function Grid() {
