e477f49026d220ff7088a72af30075fc52177b01
Matias Paradela <paradelamatias@gmail.com>
2023-02-20 11:08:16 -0300

    Add props to Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,10 @@
 import imgSrc from "../assets/images.png"
 
-export default function Card() {
-  const name = "Name";
-
+export default function Card(props) {
   return(
     <div>
       <img src={imgSrc} alt="#"/>
-      <p>{name}</p>
+      <p>{props.name}</p>
     </div>
   )
 }
