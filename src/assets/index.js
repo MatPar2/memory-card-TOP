33b427a6726eab2959b4a5bfdcecb64e497227c3
Matias Paradela <paradelamatias@gmail.com>
2023-02-22 13:40:15 -0300

    Render cards including images

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+const IMAGES = {
+  Homer: require('./Homer.png'),
+  Marge: require('./Marge.png'),
+  Bart: require('./Bart.png'),
+  Lisa: require('./Lisa.png'),
+  Maggie: require('./Maggie.png'),
+  Moe: require('./Moe.png'),
+  Ned: require('./Ned.png'),
+  Burns: require('./Burns.png'),
+  Grampa: require('./Grampa.png')
+}
+
+export default IMAGES;
