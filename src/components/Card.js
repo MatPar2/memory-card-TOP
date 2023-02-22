105c352d502fbf9d4ce7602d9999991499a18b6a
Matias Paradela <paradelamatias@gmail.com>
2023-02-22 11:11:30 -0300

    Add click handler for cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,12 @@
 import imgSrc from "../assets/images.png"
 
+function handleClick(name) {
+  console.log(name);
+}
+
 export default function Card(props) {
   return(
-    <div className="Card">
+    <div className="Card" onClick={() => handleClick(props.name)}>
       <img src={imgSrc} alt="#"/>
       <p>{props.name}</p>
     </div>
