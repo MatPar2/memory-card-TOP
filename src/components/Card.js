37ad249ebb5415ef2df2b2fbde267ccafd931bac
Matias Paradela <paradelamatias@gmail.com>
2023-02-22 11:51:10 -0300

    Add condition to handle click, if the user repeats a character

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,23 @@
 import imgSrc from "../assets/images.png"
 
+function createNewMatch() {
+  // ToDo
+}
+
 export default function Card(props) {
   function handleClick(props) {
-    props.setScore(s => s + 1)
-    
-    props.setClickedCharacters([
-      ...props.clickedCharacters,
-      props.name
-    ]);
+    if (props.clickedCharacters.some(elem => elem === props.name)) {
+      createNewMatch();
+    } else {
+      // Sum 1 to the score
+      props.setScore(s => s + 1)
+
+      // Save new character to selected ones
+      props.setClickedCharacters([
+        ...props.clickedCharacters,
+        props.name
+      ]);
+    }
   }
 
   return(
